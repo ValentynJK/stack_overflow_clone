d03be12cfae58fd99ef05f62e845cd6156f0e71e
Valentyn <valentynejk@gmail.com>
2023-09-27 20:24:11 +0200

    auth added

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <div className='h1-bold'>H1 Bold</div>
-      <div className='h2-bold'>H2 Bold</div>
-      <div className='h3-bold'>H3 Bold</div>
-    </>
-  );
-}
