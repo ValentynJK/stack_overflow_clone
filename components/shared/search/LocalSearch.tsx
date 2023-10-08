778a8e149c9f11c1a618a4a37b7c60993c167a7b
Valentyn Kolesnichenko <valentynkolesnichenko@Valentyns-MacBook-Pro.local>
2023-10-08 20:30:29 +0200

    local search for homepage

--- components/shared/search/LocalSearch.tsx
+++ components/shared/search/LocalSearch.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React from "react";
 
-const GlobalSearch = () => {
+const LocalSearch = () => {
   return (
-    <div className="relative w-full max-w-[600px] max-lg:hidden">
+    <div className="relative w-full">
       <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
         <Image
           src="/assets/icons/search.svg"
@@ -16,14 +17,14 @@ const GlobalSearch = () => {
         />
         <Input
           type="text"
-          placeholder="Search globally"
+          placeholder="Search questions..."
           value=""
           onChange={() => {}}
-          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none "
+          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
         />
       </div>
     </div>
   );
 };
 
-export default GlobalSearch;
+export default LocalSearch;
