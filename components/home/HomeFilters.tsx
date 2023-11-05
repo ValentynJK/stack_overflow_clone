b36ef0cd99346e754a623d62fffd5104b4901b3b
Valentyn Kolesnichenko <valentynkolesnichenko@Valentyns-MacBook-Pro.local>
2023-11-05 13:52:04 +0100

    implemented home tag filters

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,10 +1,27 @@
-// import { HomePageFilters } from "@/constants/filters";
+"use client";
+
+import { HomePageFilters } from "@/constants/filters";
+import { Button } from "../ui/button";
 
 const HomeFilters = () => {
+  const active = "frequent";
+
   return (
-    <>
-      <div className="mt-10 hidden flex-wrap gap-3 md:flex"></div>
-    </>
+    <div className="mt-10 hidden flex-wrap gap-3 md:flex">
+      {HomePageFilters.map(({ name, value }) => (
+        <Button
+          key={value}
+          onClick={() => {}}
+          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
+            active === value
+              ? "bg-primary-100 text-primary-500 dark:bg-dark-400"
+              : "bg-light-800 text-light-500 hover:bg-light-900 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-500"
+          }`}
+        >
+          {name}
+        </Button>
+      ))}
+    </div>
   );
 };
 
