a2a66ebf9bb916968c884a72c6a65e10a898e5e0
ValentynJK <valentynejk@gmail.com>
2023-12-13 21:28:50 +0100

    created Tags Page

---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,48 @@
+import TagCard from "@/components/cards/TagCard";
+import Filter from "@/components/shared/Filter";
+import LocalSearch from "@/components/shared/search/LocalSearch";
+import { TagFilters } from "@/constants/filters";
+import { getAllTags } from "@/lib/actions/tag.actions";
+import Link from "next/link";
+
+const Tags = async () => {
+  const { tags } = await getAllTags({});
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Tags</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center ">
+        <LocalSearch
+          route="/community"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for amazing minds"
+          otherClasses="flex-1"
+        />
+
+        <Filter
+          filters={TagFilters}
+          placeholder="Select a Filter"
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="max-md:flex"
+        />
+      </div>
+
+      <section className="mt-12 flex flex-wrap gap-4">
+        {tags.length > 0 ? (
+          tags.map((tag) => <TagCard key={tag._id} tag={tag} />)
+        ) : (
+          <div className="paragraph-regular text-dark200_light800 mx-auto max-w-xl text-center">
+            <p>No users yet</p>
+            <Link href="/sign-up" className="mt-2 font-bold text-accent-blue">
+              Join to be the first
+            </Link>
+          </div>
+        )}
+      </section>
+    </>
+  );
+};
+
+export default Tags;
--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,6 +1,7 @@
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
-import { GetTopInteractedTagsParams } from "./shared.type";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.type";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -20,6 +21,20 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "React" },
       { _id: "3", name: "JS" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// TODO: Refactor the code to utilize params
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
